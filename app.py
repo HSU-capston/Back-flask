db71712d08c191c683487588cc92355efc4754f8
SeungtaekLim <0530lim0530@gmail.com>
2025-04-28 16:59:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -155,4 +155,4 @@ if __name__ == '__main__':
     #     os.makedirs(RESULT_FOLDER)
     
     # Flask 앱 실행
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, use_reloader = False, host='0.0.0.0', port=5000)
